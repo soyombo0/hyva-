091bfbaea132c91a33ed15d08017ca5fee202c88
soyombo <soyombo0@naver.com>
2023-02-03 23:30:18 +0300

    Initial Commit

---
+++ hyvä/hyva_App.swift
@@ -0,0 +1,17 @@
+//
+//  hyva_App.swift
+//  hyvä
+//
+//  Created by Soyombo Mantaagiin on 3.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct hyva_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
